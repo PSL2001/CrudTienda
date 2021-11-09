6d7d91a8c8b4acee6996d43972010cf60b7b0916
PSL2001 <65461723+PSL2001@users.noreply.github.com>
2021-11-10 00:24:49 +0100

    Terminado conexion

--- src/Conexion.php
+++ src/Conexion.php
@@ -13,5 +13,22 @@ class Conexion {
         //1. Leemos el archivo .conf
         $fichero = dirname(__DIR__, 1)."/.conf";
         $opciones = parse_ini_file($fichero);
+        //Guardamos los datos del fichero
+        $host = $opciones['host'];
+        $bbdd = $opciones['bbdd'];
+        $user = $opciones['user'];
+        $pass = $opciones['pass'];
+        //Creamos el dns
+        $dns = "mysql:host=$host;bdname=$bbdd;charset=utf8mb4";
+
+        //Creamos la conexion
+        try {
+            self::$conexion = new PDO($dns, $user, $pass);
+            //Añade esta linea solo si estas en desarrollo
+            self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $ex) {
+            die("Error al conectar con la base de datos: ".$ex->getMessage());
+        }
+        
     }
 }
